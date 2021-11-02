1577274086881c380f6da7cb3fb5f24270ae2656
unknown <bruno4t@hpotmail.com>
2021-11-02 11:39:01 -0300

    finalização de todo html e css do site, faltando implementar apenas o js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-window.onload = function() {
+const orangeColor = "#ED3E0C"
+const whiteColor = "#F2F2F2"
 
+const Main={
+
+    init: function(){
+        this.cacheSelectors()
+        this.bindEvents();
+
+    },
+
+    cacheSelectors: function(){
+        this.$dots = document.querySelectorAll('.dot-image')
+    },
+
+    bindEvents: function(){
+
+        const self = this
+
+    },
+
+    events:{
+        
+    }
 }
+
+Main.init()
